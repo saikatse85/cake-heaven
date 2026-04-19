6e3f7f925a4bb5755441662b527e38337e189a42
saikatse85 <saikatse@gmail.com>
2026-04-19 21:54:36 +0600

    added env file

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAqhjODKcTRpCvjuIc6ORVnv4B80xyodOY",
-  authDomain: "cake-heaven-8f1e3.firebaseapp.com",
-  projectId: "cake-heaven-8f1e3",
-  storageBucket: "cake-heaven-8f1e3.firebasestorage.app",
-  messagingSenderId: "194510272846",
-  appId: "1:194510272846:web:25834fbf9615a8f8e0461f"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId:process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
